68ad0a790140e6a4010686fda21b28ab182b1184
Dario Brozovic <dario_br@gmx.de>
2017-06-14 16:48:59 +0200

    html in php geändert

--- views/Login.php
+++ views/Login.php
@@ -34,7 +34,7 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-lg-12">
-                            <form id="login-form" action="index.html" method="post" role="form" style="display: block;">
+                            <form id="login-form" action="index.php" method="post" role="form" style="display: block;">
                                 <div class="form-group">
                                     <label for="username">Username</label>
                                     <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
