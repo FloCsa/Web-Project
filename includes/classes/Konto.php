5b74ee9b496dd66e1f581b21edbee445aa38b86f
johannesfahringer <j.fahringer@hotmail.com>
2017-06-25 17:31:34 +0200

    mathe ist online

---
+++ includes/classes/Konto.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jojo
+ * Date: 25.06.2017
+ * Time: 17:20
+ */
+class Konto
+{
+
+    public static function createAccount($data)
+    {
+        $db = new Database();
+        $kontoname = $db->escapeString($data['kontoname']);
+        $straße = $db->escapeString($data['straße']);
+        $nummer = $db->escapeString($data['nummer']);
+        $plz = $db->escapeString($data['plz']);
+        $ort = $db->escapeString($data['ort']);
+        $stnummer = $db->escapeString($data['stnummer']);
+        $email = $db->escapeString($data['email']);
+
+
+        $sql = "INSERT INTO `konto`(`kontoname`,`straße`,`nummer`,`plz`,`ort`,`stnummer`,`stnummer`) VALUES('".$kontoname."','".$straße."','".$nummer."','".$plz."','".$ort."','".$stnummer."','".$email."')";
+        $db->query($sql);
+    }
+
+
+}
--- includes/views/kontoHinzufuegen.php
+++ includes/views/kontoHinzufuegen.php
@@ -2,46 +2,54 @@
 echo $this->header;
 ?>
 <div class="container">
-<div class="col-lg-12 well">
-    <div class="row">
-        <form>
-            <div class="col-sm-12">
-                <div class="form-group">
-                    <label>Kontoname</label>
-                    <input type="text" placeholder="Kontoname" class="form-control">
-                </div>
-                <div class="row">
-                    <div class="col-sm-6 form-group">
-                        <label>Straße</label>
-                        <input type="text" placeholder="Straße" class="form-control">
+    <div class="col-lg-12 well">
+        <div class="row">
+            <form>
+                <div class="col-sm-12">
+                    <div class="form-group">
+                        <label>Kontoname</label>
+                        <input type="text" name="kontoname" placeholder="Kontoname" class="form-control">
                     </div>
-                    <div class="col-sm-6 form-group">
-                        <label>Hausnummer</label>
-                        <input type="text" placeholder="Hausnummer" class="form-control">
+                    <div class="row">
+                        <div class="col-sm-6 form-group">
+                            <label>Straße</label>
+                            <input type="text" name="straße" placeholder="Straße" class="form-control">
+                        </div>
+                        <div class="col-sm-6 form-group">
+                            <label>Hausnummer</label>
+                            <input type="text" name="nummer" placeholder="Hausnummer" class="form-control">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 form-group">
+                            <label>PLZ</label>
+                            <input type="text" name="plz" placeholder="PLZ" class="form-control">
+                        </div>
+                        <div class="col-sm-6 form-group">
+                            <label>Ort</label>
+                            <input type="text" name="ort" placeholder="Ort" class="form-control">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>E-Mail</label>
+                        <input type="text" name="email" placeholder="E-Mail" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Steuer ID</label>
+                        <input type="text" name="stnummer" placeholder="Steuer ID" class="form-control">
+                    </div>
+                    <div class="Buttons">
+                        <button type="button" class="btn btn-lg btn-info"
+                                onclick="window.location.href='index.php'">Abbrechen
+                        </button>
+                        <button type="button" class="btn btn-lg btn-info"
+                                onclick="window.location.href='index.php'">Speichern
+                        </button>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-sm-6 form-group">
-                        <label>PLZ</label>
-                        <input type="text" placeholder="PLZ" class="form-control">
-                    </div>
-                    <div class="col-sm-6 form-group">
-                        <label>Ort</label>
-                        <input type="text" placeholder="Ort" class="form-control">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label>E-Mail</label>
-                    <input type="text" placeholder="E-Mail" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label>Steuer ID</label>
-                    <input type="text" placeholder="Steuer ID" class="form-control">
-                </div>
-            </div>
-        </form>
+            </form>
+        </div>
     </div>
-</div>
 </div>
 
 
